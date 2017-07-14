5dbf0b12f79eab4a200525765940c310abdc74a6
bhavani <bhava0895@gmail.com>
2017-07-14 12:54:05 +0530

    add test suite for magic

--- test.py
+++ test.py
@@ -12,5 +12,42 @@ class TestCodeFormat(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
+
+class MagicTest(unittest.TestCase):
+
+    def setUp(self):
+        """ SetUp Main's object """
+        self.main = Main()
+
+    def test_magic_with_empty_string(self):
+        """Test on empty Strings"""
+        self.assertEqual(True, self.main.magic("", ""))
+
+    def test_magic_with_jumble_empty(self):
+        """Test on empty jumble param"""
+        self.assertEqual(False, self.main.magic("", "road"))
+
+    def test_magic_with_word_empty(self):
+        """Test on empty word param"""
+        self.assertEqual(True, self.main.magic("road", ""))
+
+    def test_magic_for_false_without_wildcards(self):
+        """Test for false condition in a set of params"""
+        dictionary = {"jubmle": "word",
+                      "jumble1": "word1",
+                      "uwtaqicy": "watch"}
+        for key, value in dictionary.items:
+            self.assertEqual(False, self.main.magic(key, value))
+
+    def test_magic_for_false_without_wildcards(self):
+        """Test for true condition in a set of params"""
+        dictionary = {"edzlatsh": "hazel",
+                      "code": "code"
+                      }
+        for key, value in dictionary.items():
+            with self.subTest(key=key, value=value):
+                self.assertEqual(True, self.main.magic(key, value))
+
+
 if __name__ == '__main__':
     unittest.main()
