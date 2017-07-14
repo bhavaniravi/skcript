cca77be3069db9ac818be20742eb19c231f02eef
bhavani <bhava0895@gmail.com>
2017-07-14 15:03:30 +0530

    add test suite for longest

--- skcript.py
+++ skcript.py
@@ -2,6 +2,9 @@ from collections import Counter
 
 
 class Main:
+    """
+    Main class with properties to find magic and longest
+    """
     def magic(self, jumble, word):
         # condition 1
         if len(jumble) < len(word):
@@ -9,13 +12,15 @@ class Main:
         counter = Counter(word) - Counter(jumble)
         if '?' not in jumble:
             return not counter
-        return sum(counter.itervalues()) <= jumble.count('?')
+        return sum(counter.values()) <= jumble.count('?')
 
     def longest(self, jumble):
+        # if not jumble:
+        #     return ""
         with open("enable1.txt") as f:
             content = f.readlines()
             content = [c.strip() for c in content]
-            max_word = content[0]
+            max_word = ""
             for index, c in enumerate(content):
                 if self.magic(jumble, c) and len(max_word) < len(c):
                     max_word = c
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from skcript import Main
 import unittest
 import pep8
+import random
 
 
 class TestCodeFormat(unittest.TestCase):
@@ -14,6 +15,9 @@ class TestCodeFormat(unittest.TestCase):
 
 
 class MagicTest(unittest.TestCase):
+    """
+    Unit test for magic property of main method
+    """
 
     def setUp(self):
         """ SetUp Main's object """
@@ -32,21 +36,76 @@ class MagicTest(unittest.TestCase):
         self.assertEqual(True, self.main.magic("road", ""))
 
     def test_magic_for_false_without_wildcards(self):
-        """Test for false condition in a set of params"""
-        dictionary = {"jubmle": "word",
-                      "jumble1": "word1",
-                      "uwtaqicy": "watch"}
-        for key, value in dictionary.items:
-            self.assertEqual(False, self.main.magic(key, value))
+        """
+        Test for true condition in a set of params
+        The test has n words each is shuffled and tested across words,
+        after removing few indexes
+        """
+        words = ("python", "jumble", "easy", "difficult",
+                 "answer",  "xylophone")
+        for word in words:
+            jumble = ''.join(random.sample(word, len(word)))[2:]
+            with self.subTest(jumble=jumble, word=word):
+                self.assertEqual(False, self.main.magic(jumble, word))
 
-    def test_magic_for_false_without_wildcards(self):
-        """Test for true condition in a set of params"""
-        dictionary = {"edzlatsh": "hazel",
-                      "code": "code"
-                      }
-        for key, value in dictionary.items():
-            with self.subTest(key=key, value=value):
-                self.assertEqual(True, self.main.magic(key, value))
+    def test_magic_for_true_without_wildcards(self):
+        """
+        Test for true condition in a set of params
+        The test has n words each is shuffled
+        and tested across words
+        """
+        words = ("python", "jumble", "easy", "difficult",
+                 "answer",  "xylophone")
+
+        for word in words:
+            jumble = ''.join(random.sample(word, len(word)))
+            with self.subTest(jumble=jumble, word=word):
+                self.assertEqual(True, self.main.magic(jumble, word))
+
+    def test_magic_for_true_with_wildcards(self):
+        words = {"p???????": "python",
+                 "j??????": "jumble",
+                 "e???": "easy",
+                 "d????????t": "difficult",
+                 "a?????r": "answer"}
+        for jumble, word in words.items():
+            with self.subTest(jumble=jumble, word=word):
+                self.assertEqual(True, self.main.magic(jumble, word))
+
+    def test_magic_for_false_with_wildcards(self):
+        words = {"p???": "python",
+                 "j???": "jumble",
+                 "e??": "easy",
+                 "d???": "difficult",
+                 "a???": "answer"}
+        for jumble, word in words.items():
+            with self.subTest(jumble=jumble, word=word):
+                self.assertEqual(False, self.main.magic(jumble, word))
+
+
+def find_longest_word_in_enable():
+    with open("enable1.txt") as f:
+        content = f.readlines()
+        content = [c.strip() for c in content]
+        max_length_word = max(content, key=len)
+        print(max_length_word)
+        return len(max_length_word), max_length_word
+
+
+class LongestTest(unittest.TestCase):
+    def setUp(self):
+        self.main = Main()
+
+    def test_empty_jumble(self):
+        self.assertEqual("", self.main.longest(""))
+
+    def test_longest_wildcard(self):
+        max_length_word = "ethylenediaminetetraacetates"
+        max_len = len(max_length_word)
+
+        # uncomment if content of enable1.txt is changed
+        # max_len,max_length_word = find_longest_word_in_enable()
+        self.assertEqual(max_length_word, self.main.longest("?" * max_len))
 
 
 if __name__ == '__main__':
